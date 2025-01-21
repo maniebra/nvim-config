20d39fe8166260fcd0ecc126411eba813ca81187
maniebra <mani.ebra@gmail.com>
2025-01-21 19:16:07 +0330

    fixed some issues regarding the keymaps configs

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -7,7 +7,7 @@ vim.keymap.set('i', 'jk', '<ESC>', { noremap = true, silent = true, desc = "Exit
 vim.keymap.set('n', '<C-G><C-A>', ':Git add .<CR>', { noremap = true, silent = true, desc = "Git add: all files in currect directory"})
 vim.keymap.set('n', '<C-G><C-P>', ':Git push<CR>', { noremap = true, silent = true, desc = "Git push" })
 vim.keymap.set('n', '<C-G><C-S>', function()
-    local commit_message = vim.fn.input('Commit message: ', '# ')
+    local commit_message = vim.fn.input('Commit message: ', '#')
     if commit_message ~= '' then
         vim.cmd('Git commit -m "' .. commit_message .. '"')
     else
@@ -15,9 +15,9 @@ vim.keymap.set('n', '<C-G><C-S>', function()
     end
 end, { noremap = true, silent = true, desc = "Git commit with dynamic message" })
 
-vim.keymap.set('n', '<F5>', ':make<CR>', { noremap = true, silent = true, desc = "Execute `all` in Makefile" })
+vim.keymap.set('n', '<F5>', ':make<CR>', { noremap = true, silent = true, desc = "Execute all in Makefile" })
 
-vim.keymap.set('n', '<C-T><C-D>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true, "Open NvimTree file tree" })
+vim.keymap.set('n', '<C-T><C-D>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true, desc = "Open NvimTree file tree" })
 
 vim.keymap.set('n', '<C-S>', ':w<CR>', { noremap = true, silent = true, desc = "Save file" })
 
@@ -25,4 +25,4 @@ vim.keymap.set('n', "<c-`>", ':FloatermToggle<CR>', {noremap = true, silent = tr
 
 -- TERMINAL
 vim.keymap.set('t', '<ESC>', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true, desc = "Exit terminal" })
-vim.keymap.set('t', 'jk', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true, "Exit terminal" })
+vim.keymap.set('t', 'jk', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true, desc = "Exit terminal" })
