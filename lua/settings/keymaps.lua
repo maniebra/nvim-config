32199555bf05d60a959a23431d6f86d0d1f9f1cb
maniebra <mani.ebra@gmail.com>
2025-01-21 19:04:11 +0330

    improved git commit shortcut

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -5,8 +5,15 @@ vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
 
 -- NORMAL
 vim.api.nvim_set_keymap('n', '<C-G><C-A>', ':Git add .<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-G><C-S>', ':Git commit -m "# "', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-G><C-P>', ':Git push<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-G><C-S>', function()
+    local commit_message = vim.fn.input('Commit message: ', '# ')
+    if commit_message ~= '' then
+        vim.cmd('Git commit -m "' .. commit_message .. '"')
+    else
+        print('Commit canceled.')
+    end
+end, { noremap = true, silent = true, desc = "Git commit with dynamic message" })
 
 vim.api.nvim_set_keymap('n', '<F5>', ':make<CR>', { noremap = true, silent = true })
 
