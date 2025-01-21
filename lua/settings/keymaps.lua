7cd7d2fbf9ba4a5a9e12b0c91121bf4cd71c0990
maniebra <mani.ebra@gmail.com>
2025-01-21 19:13:03 +0330

    refactored the whole keymaps config

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -1,11 +1,11 @@
 -- SHORCUTS
 -- INSERT
-vim.api.nvim_set_keymap('i', '<C-S>', '<ESC>:w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-S>', '<ESC>:w<CR>', { noremap = true, silent = true, desc = "Save file" })
+vim.keymap.set('i', 'jk', '<ESC>', { noremap = true, silent = true, desc = "Exit insert mode" })
 
 -- NORMAL
-vim.api.nvim_set_keymap('n', '<C-G><C-A>', ':Git add .<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-G><C-P>', ':Git push<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-G><C-A>', ':Git add .<CR>', { noremap = true, silent = true, desc = "Git add: all files in currect directory"})
+vim.keymap.set('n', '<C-G><C-P>', ':Git push<CR>', { noremap = true, silent = true, desc = "Git push" })
 vim.keymap.set('n', '<C-G><C-S>', function()
     local commit_message = vim.fn.input('Commit message: ', '# ')
     if commit_message ~= '' then
@@ -15,14 +15,14 @@ vim.keymap.set('n', '<C-G><C-S>', function()
     end
 end, { noremap = true, silent = true, desc = "Git commit with dynamic message" })
 
-vim.api.nvim_set_keymap('n', '<F5>', ':make<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F5>', ':make<CR>', { noremap = true, silent = true, desc = "Execute `all` in Makefile" })
 
-vim.api.nvim_set_keymap('n', '<C-T><C-D>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-T><C-D>', ':NvimTreeFindFile<CR>', { noremap = true, silent = true, "Open NvimTree file tree" })
 
-vim.api.nvim_set_keymap('n', '<C-S>', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-S>', ':w<CR>', { noremap = true, silent = true, desc = "Save file" })
 
-vim.api.nvim_set_keymap('n', "<c-`>", ':FloatermToggle<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', "<c-`>", ':FloatermToggle<CR>', {noremap = true, silent = true, desc = "Open floating terminal" })
 
 -- TERMINAL
-vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', 'jk', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true })
+vim.keymap.set('t', '<ESC>', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true, desc = "Exit terminal" })
+vim.keymap.set('t', 'jk', '<C-\\><C-N>:q<CR>', { noremap = true, silent = true, "Exit terminal" })
