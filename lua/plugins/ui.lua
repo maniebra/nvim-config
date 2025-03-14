8b3d44d4b4a4f39bfd43e1c20203d08d74991c54
maniebra <mani.ebra@gmail.com>
2025-03-14 22:57:01 +0330

    added nui and its cool notification

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,6 +9,7 @@ return {
   { "nvzone/volt" , lazy = true },
   { "nvzone/menu" , lazy = true },
   { "nvzone/minty", cmd = { "Huefy", "Shades" } },
+  { "nvzone/showkeys", cmd = "ShowkeysToggle" },
   {
     'nvimdev/dashboard-nvim',
     event = 'VimEnter',
@@ -25,6 +26,22 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
   },
-  "mcookly/bidi.nvim"
+  "mcookly/bidi.nvim",
+
+   {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+       }
+  }
 
 }
